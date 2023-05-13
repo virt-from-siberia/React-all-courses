eb2a6b858ec5295fd242921644038cfb5127d804
Aleksey Elchin <alonso85@mail.ru>
2023-05-13 20:03:56 +1000

    react typescript

---
+++ React and TypeScript, v2/name-badges-main/src/components/application.tsx
@@ -0,0 +1,10 @@
+// import ControlPanel from './controls';
+import NameBadge from './name-badge';
+
+const Application = () => (
+  <main className="application">
+    <NameBadge name="Aleksey" />
+  </main>
+);
+
+export default Application;
---
+++ React and TypeScript, v2/name-badges-main/src/components/name-badge.tsx
@@ -0,0 +1,18 @@
+const addTwo = (n: number): number => n + 2;
+
+const NameBadge = (props: { name: string }) => {
+  return (
+    <section className="badge">
+      <header className="badge-header">
+        <h1 className="text-5xl">HELLO</h1>
+        <p>My name is…</p>
+      </header>
+      <div className="badge-body">
+        <p className="badge-name">{props.name}</p>
+      </div>
+      <footer className="badge-footer" />
+    </section>
+  );
+};
+
+export default NameBadge;
