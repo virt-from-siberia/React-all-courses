9b52727adaa0ff5ca09e7db396af421cdc17a544
Aleksey Elchin <alonso85@mail.ru>
2023-05-15 04:25:13 +1000

    react typescript

---
+++ React and TypeScript, v2/typing-children-main/src/components/application.tsx
@@ -0,0 +1,51 @@
+/**
+ * Things you could try:
+ *
+ * JSX.Element;
+ * JSX.Element | JSX.Element[];
+ * React.ReactNode;
+ * React.ReactChildren;
+ * React.ReactChild[];
+ */
+
+import { PropsWithChildren } from 'react';
+
+type BoxProps = PropsWithChildren<{
+  style: React.CSSProperties;
+}>;
+
+const Box = ({ children, style }: BoxProps) => {
+  return (
+    <section
+      className="m-4"
+      style={{
+        padding: '1em',
+        border: '5px solid purple',
+        margin: '3em',
+        ...style,
+      }}
+    >
+      {children}
+    </section>
+  );
+};
+
+const Application = () => {
+  return (
+    <main className="m-8">
+      <Box>
+        Just a string.
+        <p>Some HTML that is not nested.</p>
+        <Box>
+          <h2>Another React component with one child.</h2>
+        </Box>
+        <Box>
+          <h2 className="mb-4">A nested React component with two children.</h2>
+          <p>The second child.</p>
+        </Box>
+      </Box>
+    </main>
+  );
+};
+
+export default Application;
