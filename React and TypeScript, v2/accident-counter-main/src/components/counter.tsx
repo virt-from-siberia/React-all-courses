bcf0ceea3cc17000e19fae04fa221a0255d8b1f9
Aleksey Elchin <alonso85@mail.ru>
2023-05-15 17:02:17 +1000

    react typescript

--- React and TypeScript, v2/accident-counter-main/src/components/counter.tsx
+++ React and TypeScript, v2/accident-counter-main/src/components/counter.tsx
@@ -1,12 +1,25 @@
+import { useState } from 'react';
+
 const Counter = () => {
+  const [count, setCount] = useState<number>(0);
+
+  const handleClick = () => {
+    console.log('handleClick', handleClick);
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+  };
+
   return (
     <section className="flex flex-col items-center w-2/3 gap-8 p-8 bg-white border-4 shadow-lg border-primary-500">
       <h1>Days Since the Last Accident</h1>
-      <p className="text-6xl">0</p>
+      <p className="text-6xl">{count}</p>
       <div className="flex gap-2">
-        <button>➖ Decrement</button>
+        <button onClick={() => setCount((count) => count + 1)}>
+          ➖ Decrement
+        </button>
         <button>🔁 Reset</button>
-        <button>➕ Increment</button>
+        <button onClick={handleClick}>➕ Increment</button>
       </div>
       <div>
         <form onSubmit={(e) => e.preventDefault()}>
