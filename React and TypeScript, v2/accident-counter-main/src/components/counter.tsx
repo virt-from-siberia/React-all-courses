1299aef5a706644bca9e4ecf09caadf785818c31
Aleksey Elchin <alonso85@mail.ru>
2023-05-15 22:17:20 +1000

    react typescript

--- React and TypeScript, v2/accident-counter-main/src/components/counter.tsx
+++ React and TypeScript, v2/accident-counter-main/src/components/counter.tsx
@@ -1,29 +1,38 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const Counter = () => {
   const [count, setCount] = useState<number>(0);
+  const [draftCount, setDraftCount] = useState<number>(count);
 
-  const handleClick = () => {
-    console.log('handleClick', handleClick);
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-  };
+  useEffect(() => {
+    count;
+  }, [count]);
 
   return (
     <section className="flex flex-col items-center w-2/3 gap-8 p-8 bg-white border-4 shadow-lg border-primary-500">
       <h1>Days Since the Last Accident</h1>
       <p className="text-6xl">{count}</p>
       <div className="flex gap-2">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count - 1)}>
           ➖ Decrement
         </button>
         <button>🔁 Reset</button>
-        <button onClick={handleClick}>➕ Increment</button>
+        <button onClick={() => setCount((count) => count + 1)}>
+          ➕ Increment
+        </button>
       </div>
       <div>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <input type="number" value={0} />
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setCount(draftCount);
+          }}
+        >
+          <input
+            type="number"
+            value={draftCount}
+            onChange={(e) => setDraftCount(e.target.valueAsNumber)}
+          />
           <button type="submit">Update Counter</button>
         </form>
       </div>
