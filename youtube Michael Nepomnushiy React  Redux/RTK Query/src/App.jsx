15e72f6788418514ba213caa336e6414e15e3784
Aleksey Elchin <alonso85@mail.ru>
2023-06-19 13:14:56 +1000

    RTK Query

--- youtube Michael Nepomnushiy React  Redux/RTK Query/src/App.jsx
+++ youtube Michael Nepomnushiy React  Redux/RTK Query/src/App.jsx
@@ -1,14 +1,41 @@
 import { useState } from "react";
-import { useGetGoodsQuery } from "./redux";
+import {
+  useGetGoodsQuery,
+  useAddProductMutation,
+  useDeleteProductMutation,
+} from "./redux";
 
 function App() {
-  const [count, setCount] = useState();
+  const [count, setCount] = useState("");
+  const [newProduct, setNewProduct] = useState("");
+
   const { data = [], isLoading } = useGetGoodsQuery(count);
+  const [addProduct, { isErrorUpdate }] = useAddProductMutation();
+  const [deleteProduct, { isErrorDelete }] = useDeleteProductMutation();
+
+  const handleAddProduct = async () => {
+    if (newProduct) {
+      await addProduct({
+        name: newProduct,
+      }).unwrap();
+      setNewProduct("");
+    }
+  };
+
+  const handleDeleteProduct = async (id) => await deleteProduct(id).unwrap();
 
   if (isLoading) return <h1>Loading...</h1>;
 
   return (
     <div>
+      <div>
+        <input
+          type="text"
+          value={newProduct}
+          onChange={(e) => setNewProduct(e.target.value)}
+        />
+        <button onClick={handleAddProduct}> add product</button>
+      </div>
       <div>
         <select value={count} onChange={(e) => setCount(e.target.value)}>
           <option value="''">all</option>
@@ -19,7 +46,11 @@ function App() {
       </div>
       <ul>
         {data.map((i) => {
-          return <div key={i.id}>{i.name}</div>;
+          return (
+            <li key={i.id} onClick={() => handleDeleteProduct(i.id)}>
+              {i.name}
+            </li>
+          );
         })}
       </ul>
     </div>
--- youtube Michael Nepomnushiy React  Redux/RTK Query/src/redux/goodsApi.js
+++ youtube Michael Nepomnushiy React  Redux/RTK Query/src/redux/goodsApi.js
@@ -2,12 +2,39 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const goodsApi = createApi({
   reducerPath: "goodsApi",
+  tagTypes: ["Products"],
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3001/" }),
   endpoints: (build) => ({
     getGoods: build.query({
       query: (limit = "") => `goods?${limit && `_limit=${limit}`}`,
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map(({ id }) => ({ type: "Products", id })),
+              { type: "Products", id: "LIST" },
+            ]
+          : [{ type: "Products", id: "LIST" }],
+    }),
+    addProduct: build.mutation({
+      query: (body) => ({
+        url: "goods",
+        method: "POST",
+        body,
+      }),
+      invalidatesTags: [{ type: "Products", id: "LIST" }],
+    }),
+    deleteProduct: build.mutation({
+      query: (id) => ({
+        url: `goods/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: [{ type: "Products", id: "LIST" }],
     }),
   }),
 });
 
-export const { useGetGoodsQuery } = goodsApi;
+export const {
+  useGetGoodsQuery,
+  useAddProductMutation,
+  useDeleteProductMutation,
+} = goodsApi;
