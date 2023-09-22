4b092e9ed8ccb2b02a083b423138909587a1f2e3
Aleksey Elchin <alonso85@mail.ru>
2023-09-23 04:32:26 +0900

    react hook form

--- youtube React Hook Form tutorial/react-hoock-form/src/components/YoutubeForm.tsx
+++ youtube React Hook Form tutorial/react-hoock-form/src/components/YoutubeForm.tsx
@@ -8,32 +8,40 @@ type FormValuesType = {
   username: string;
   email: string;
   channel: string;
+  social: {
+    twitter: string;
+    facebook: string;
+  };
 };
 
 export const YouTubeForm = () => {
   // default values
-  // const form = useForm<FormValuesType>({
-  //   defaultValues: {
-  //     username: "Batman",
-  //     email: "",
-  //     channel: "",
-  //   },
-  // });
-
   const form = useForm<FormValuesType>({
-    defaultValues: async () => {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/users/1"
-      );
-      const data = await response.json();
-
-      return {
-        username: "Batman",
-        email: data.email,
-        channel: "",
-      };
+    defaultValues: {
+      username: "Batman",
+      email: "",
+      channel: "",
+      social: {
+        twitter: "",
+        facebook: "",
+      },
     },
   });
+
+  // const form = useForm<FormValuesType>({
+  //   defaultValues: async () => {
+  //     const response = await fetch(
+  //       "https://jsonplaceholder.typicode.com/users/1"
+  //     );
+  //     const data = await response.json();
+
+  //     return {
+  //       username: "Batman",
+  //       email: data.email,
+  //       channel: "",
+  //     };
+  //   },
+  // });
 
   const { register, handleSubmit, control, formState } = form;
   const { errors } = formState;
@@ -71,6 +79,7 @@ export const YouTubeForm = () => {
                 value: /^[\w\.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/,
                 message: "Invalid email address",
               },
+              required: { message: "Email is required", value: true },
               validate: {
                 blackList: (fieldValue) => {
                   return (
@@ -86,14 +95,18 @@ export const YouTubeForm = () => {
 
         <div className="form-control">
           <label htmlFor="channel">Channel</label>
-          <input
-            type="text"
-            id="channel"
-            {...register("channel", {
-              required: { message: "Username is required", value: true },
-            })}
-          />
-          <p className="error">{errors.channel?.message}</p>
+          <input type="text" id="channel" {...register("channel")} />
+          <p className="error">{errors.username?.message}</p>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="twitter">Twitter</label>
+          <input type="text" id="channel" {...register("social.twitter")} />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="facebook">Facebook</label>
+          <input type="text" id="channel" {...register("social.facebook")} />
         </div>
 
         <button>Submit</button>
