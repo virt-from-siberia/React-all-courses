1a3775558613797ca81b6c2600420d3223513903
Aleksey Elchin <alonso85@mail.ru>
2023-09-24 06:55:46 +0900

    react tables

---
+++ youtube React Tables tutorial/tables/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+import { BasicTable } from "./components/BasicTable";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <BasicTable />
+      </div>
+    </>
+  );
+}
+
+export default App;
