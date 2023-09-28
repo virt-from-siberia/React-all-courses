f2c80599966630361004d789e0870f0e35171c99
Aleksey Elchin <alonso85@mail.ru>
2023-09-29 03:20:56 +0900

    chakra ui

---
+++ youtube Chakra UI Crash Course/Chakra-UI-Crash-Course/src/pages/Dashboard.jsx
@@ -0,0 +1,32 @@
+import { Heading, Text, Container, Box } from "@chakra-ui/react";
+
+export default function Dashboard() {
+  const boxStyles = {
+    bg: "tomato",
+    color: "white",
+    p: "20px",
+    borderRadius: "10px",
+    ":hover": {
+      color: "black",
+      bg: "blue.200",
+    },
+  };
+
+  return (
+    <Container as="section" maxWidth="4xl" border="1px solid black" py="20px">
+      <Heading my="30px" p="10px">
+        Chakra uo components
+      </Heading>
+      <Text ml="30px">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. A, nihil?
+      </Text>
+      <Text ml="30px" color="blue.400" fontWeight="bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. A, nihil?
+      </Text>
+      <Box my="30px" p="20px" bg="orange">
+        <Text color="white">This is a box</Text>
+      </Box>
+      <Box sx={boxStyles}>Hello virt</Box>
+    </Container>
+  );
+}
