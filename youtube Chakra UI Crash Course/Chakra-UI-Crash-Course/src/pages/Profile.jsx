a87f1b64261a5c85990c17ece0611a5a3f30e5e7
Aleksey Elchin <alonso85@mail.ru>
2023-09-29 22:47:56 +0900

    chakra ui

--- youtube Chakra UI Crash Course/Chakra-UI-Crash-Course/src/pages/Profile.jsx
+++ youtube Chakra UI Crash Course/Chakra-UI-Crash-Course/src/pages/Profile.jsx
@@ -1,5 +1,74 @@
+import {
+  Tab,
+  TabList,
+  TabPanels,
+  Tabs,
+  TabPanel,
+  ListItem,
+  ListIcon,
+  List,
+} from "@chakra-ui/react";
+
+import {
+  AtSignIcon,
+  CalendarIcon,
+  ChatIcon,
+  CheckCircleIcon,
+  EditIcon,
+  EmailIcon,
+  StarIcon,
+  WarningIcon,
+} from "@chakra-ui/icons";
+
 export default function Profile() {
   return (
-    <div>Profile</div>
-  )
+    <Tabs mt="40px" p="20px" colorScheme="purple">
+      <TabList>
+        <Tab _selected={{ color: "white", bg: "purple.500" }}>Account info</Tab>
+        <Tab>Task history</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel>
+          <List spacing={4}>
+            <ListItem>
+              <ListIcon as={EmailIcon} />
+              Email: alonso85@mail.ru
+            </ListItem>
+            <ListItem>
+              <ListIcon as={ChatIcon} />
+              Lorem ipsum dolor sit amet.qas
+            </ListItem>
+            <ListItem>
+              <ListIcon as={StarIcon} />
+              Lorem ipsum dolor sit amet consectetur adipisicing.
+            </ListItem>
+          </List>
+        </TabPanel>
+        <TabPanel>
+          <List spacing={4}>
+            <ListItem>
+              <ListIcon as={CheckCircleIcon} color="green.400" />
+              Lorem ipsum dolor sit amet.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={CheckCircleIcon} color="green.400" />
+              Lorem ipsum dolor sit amet.qas
+            </ListItem>
+            <ListItem>
+              <ListIcon as={WarningIcon} color="red.400" />
+              Lorem ipsum dolor sit amet consectetur adipisicing.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={CheckCircleIcon} color="green.400" />
+              Lorem ipsum dolor sit amet consectetur adipisicing.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={WarningIcon} color="red.400" />
+              Lorem ipsum dolor sit amet consectetur adipisicing.
+            </ListItem>
+          </List>
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
+  );
 }
