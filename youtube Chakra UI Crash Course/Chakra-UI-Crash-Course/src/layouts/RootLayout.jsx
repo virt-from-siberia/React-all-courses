ec08f449a4d110e39de534b4ceae8dfada1eacf3
Aleksey Elchin <alonso85@mail.ru>
2023-10-01 01:45:41 +0900

    chakra ui

--- youtube Chakra UI Crash Course/Chakra-UI-Crash-Course/src/layouts/RootLayout.jsx
+++ youtube Chakra UI Crash Course/Chakra-UI-Crash-Course/src/layouts/RootLayout.jsx
@@ -9,7 +9,7 @@ export default function RootLayout() {
       <GridItem
         as="aside"
         colSpan={{ base: 6, lg: 2, xl: 1 }}
-        bg="purple.400"
+        bg="brand.700"
         minHeight={{
           lg: "100vh",
         }}
--- youtube Chakra UI Crash Course/Chakra-UI-Crash-Course/src/main.jsx
+++ youtube Chakra UI Crash Course/Chakra-UI-Crash-Course/src/main.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 import App from "./App";
 
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const fonts = {
+  body: "system-ui, sans-serif",
+  heading: "system-ui, sans-serif",
+  mono: "Menlo, Monaco, Lucida Console, monospace",
+};
+
+const theme = extendTheme({ colors, fonts });
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
