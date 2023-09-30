93fff99f8837d6c945c9cd82ce05f88141d0067a
Aleksey Elchin <alonso85@mail.ru>
2023-09-30 16:31:45 +0900

    chakra ui

--- youtube Chakra UI Crash Course/Chakra-UI-Crash-Course/src/components/Navbar.jsx
+++ youtube Chakra UI Crash Course/Chakra-UI-Crash-Course/src/components/Navbar.jsx
@@ -7,9 +7,25 @@ import {
   Heading,
   Spacer,
   Text,
+  useToast,
 } from "@chakra-ui/react";
+import { UnlockIcon } from "@chakra-ui/icons";
 
 export const Navbar = () => {
+  const toast = useToast();
+
+  const showToast = () => {
+    toast({
+      title: "Logout",
+      description: "You have been logged out",
+      // status: "success",
+      duration: 5000,
+      isClosable: true,
+      position: "top",
+      icon: <UnlockIcon />,
+    });
+  };
+
   return (
     <Flex as="nav" p="10px" alignItems="center" gap="10px" mb="40px">
       <Heading as="h1">Hello world</Heading>
@@ -17,7 +33,9 @@ export const Navbar = () => {
       <HStack spacing="20px">
         <Box bg="gray.200" p="10px"></Box>
         <Text>alonso85@mial.ru</Text>
-        <Button colorScheme="teal">Logout</Button>
+        <Button colorScheme="teal" onClick={showToast}>
+          Logout
+        </Button>
       </HStack>
     </Flex>
   );
