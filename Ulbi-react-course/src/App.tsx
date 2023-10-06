7fa66f1594a8b51df11a7aea74f231cbc959f7e3
Aleksey Elchin <alonso85@mail.ru>
2023-10-07 06:51:34 +0900

    ubi

--- Ulbi-react-course/src/App.tsx
+++ Ulbi-react-course/src/App.tsx
@@ -4,13 +4,14 @@ import { Route, Routes, Link } from "react-router-dom";
 import { MainPage } from "./pages/MainPage/MainPage.async";
 import { AboutPage } from "./pages/AboutPage/AboutPage.async";
 
-import "./index.scss";
+import "./styles/index.scss";
 
 export const App = () => {
   return (
-    <div className="app">
+    <div className="app dark">
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
+      Hello world
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path={"./"} element={<MainPage />} />
