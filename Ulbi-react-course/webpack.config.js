66b02a0d2bd8513de64f9ce28d65a7c0b857b87d
Aleksey Elchin <alonso85@mail.ru>
2023-10-06 16:39:06 +0900

    ubiy

--- Ulbi-react-course/webpack.config.js
+++ Ulbi-react-course/webpack.config.js
@@ -1,9 +1,19 @@
 const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
-  entry: "./src/index.js",
+  mode: "development", // "production"
+  entry: path.resolve(__dirname, "src", "index.js"),
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "build"),
+    clean: true,
   },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+    new webpack.ProgressPlugin(),
+  ],
 };
