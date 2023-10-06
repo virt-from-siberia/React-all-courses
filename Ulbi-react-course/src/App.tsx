86cf0c90b047153e85e65b94967b9a8d3ab7be75
Aleksey Elchin <alonso85@mail.ru>
2023-10-07 06:20:15 +0900

    ubi

--- Ulbi-react-course/src/App.tsx
+++ Ulbi-react-course/src/App.tsx
@@ -1,12 +1,22 @@
-import { Counter } from "./components/Counter";
+import { Suspense } from "react";
+import { Route, Routes, Link } from "react-router-dom";
+
+import { MainPage } from "./pages/MainPage/MainPage.async";
+import { AboutPage } from "./pages/AboutPage/AboutPage.async";
 
 import "./index.scss";
 
 export const App = () => {
   return (
     <div className="app">
-      ASDASDAS
-      <Counter />
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"./"} element={<MainPage />} />
+          <Route path={"./about"} element={<AboutPage />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
