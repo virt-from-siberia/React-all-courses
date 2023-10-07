f3669fb143561b9124037d22884c6d1c85a2b0ba
Aleksey Elchin <alonso85@mail.ru>
2023-10-07 16:05:26 +0900

    ubi

--- Ulbi-react-course/src/index.tsx
+++ Ulbi-react-course/src/index.tsx
@@ -2,12 +2,15 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import { App } from "./App";
+import ThemeProvider from "./theme/ThemeProvider";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <BrowserRouter>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </BrowserRouter>
 );
---
+++ Ulbi-react-course/src/theme/ThemeProvider.tsx
@@ -0,0 +1,29 @@
+import { useMemo, useState } from "react";
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "./ThemeContext";
+
+const defaultTheme =
+  (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT;
+
+type IOwnProps = {
+  children: React.ReactNode;
+};
+
+const ThemeProvider: React.FC<IOwnProps> = ({ children }) => {
+  const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+  const defaultProps = useMemo(
+    () => ({
+      theme,
+      setTheme,
+    }),
+    [theme]
+  );
+
+  return (
+    <ThemeContext.Provider value={defaultProps}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
