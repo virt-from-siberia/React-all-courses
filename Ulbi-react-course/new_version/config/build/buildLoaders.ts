c1cda73e163dbb06473d980398960e115b71ab86
Aleksey Elchin <alonso85@mail.ru>
2023-10-08 18:17:03 +0900

    ubi

--- Ulbi-react-course/new_version/config/build/buildLoaders.ts
+++ Ulbi-react-course/new_version/config/build/buildLoaders.ts
@@ -3,6 +3,17 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import { BuildOptions } from "./types/config";
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"],
+      },
+    },
+  };
+
   const svgLoader = {
     test: /\.svg$/i,
     issuer: /\.[jt]sx?$/,
@@ -44,5 +55,5 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     ],
   };
 
-  return [typescriptLoader, cssLoader, fileLoader, svgLoader];
+  return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader];
 }
---
+++ Ulbi-react-course/new_version/src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,36 @@
+import { classNames } from "./classNames";
+
+describe("classNames", () => {
+  test("with only first param", () => {
+    expect(classNames("someClass")).toBe("someClass");
+  });
+  test("with additional class", () => {
+    expect(classNames("someClass", {}, ["class1", "class2"])).toBe(
+      "someClass class1 class2"
+    );
+  });
+  test("with mods", () => {
+    expect(
+      classNames("someClass", { hovered: true, scrollable: true }, [
+        "class1",
+        "class2",
+      ])
+    ).toBe("someClass class1 class2 hovered scrollable");
+  });
+  test("with mods false", () => {
+    expect(
+      classNames("someClass", { hovered: true, scrollable: false }, [
+        "class1",
+        "class2",
+      ])
+    ).toBe("someClass class1 class2 hovered");
+  });
+  test("with mods undefined false", () => {
+    expect(
+      classNames("someClass", { hovered: undefined, scrollable: false }, [
+        "class1",
+        "class2",
+      ])
+    ).toBe("someClass class1 class2");
+  });
+});
