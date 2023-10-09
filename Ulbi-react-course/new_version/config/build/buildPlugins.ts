f42c737a0a3a1a0c6fddb0039f20f365990c267e
Aleksey Elchin <alonso85@mail.ru>
2023-10-09 20:38:26 +0900

    ubi

--- Ulbi-react-course/new_version/config/build/buildPlugins.ts
+++ Ulbi-react-course/new_version/config/build/buildPlugins.ts
@@ -1,7 +1,9 @@
-import HtmlWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
-import { BuildOptions } from "./types/config";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
+
+import { BuildOptions } from "./types/config";
 
 export function buildPlugins({
   paths,
@@ -20,5 +22,8 @@ export function buildPlugins({
       __IS_DEV__: JSON.stringify(isDev),
     }),
     new webpack.HotModuleReplacementPlugin(),
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+    }),
   ];
 }
