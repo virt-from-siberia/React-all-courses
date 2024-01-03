964d6a8e7e6789568ae89a5c860b1bc66d5d76ed
Aleksey Elchin <alonso85@mail.ru>
2024-01-04 06:27:02 +0900

    react signals

--- youtube WebDevSimplified React signals/react-signals/src/Navbar.jsx
+++ youtube WebDevSimplified React signals/react-signals/src/Navbar.jsx
@@ -1,13 +1,16 @@
+import { computed } from "@preact/signals-react";
 import { todos } from "./TodoList";
 
 export const Navbar = () => {
   console.log("Render Navbar");
 
+  const completedTodosCount = computed(() => {
+    return todos.value.filter((todo) => todo.completed).length;
+  });
+
   return (
     <nav className="navbar">
-      <div>
-        completed: {todos.value.filter((todo) => todo.completed).length}
-      </div>
+      <div>completed: {completedTodosCount}</div>
       <a href="/">Todos</a>
       <a href="/account">Account</a>
     </nav>
