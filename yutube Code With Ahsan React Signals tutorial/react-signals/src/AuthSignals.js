9d5b63e673a16c9ce5759d95cdbc80d7d35cd3e2
Aleksey Elchin <alonso85@mail.ru>
2024-01-05 00:28:13 +0900

    react signals

--- yutube Code With Ahsan React Signals tutorial/react-signals/src/AuthSignals.js
+++
@@ -1,7 +0,0 @@
-import { signal, computed } from "@preact/signals-react";
-
-export const userSignal = signal(null);
-
-export const isLoggedInSignal = computed(() => {
-  return !!userSignal.value;
-});
